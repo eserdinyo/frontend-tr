c667fb03b6ff28423e2af9240204155e426ccac2
eserdinyo <muhammeteser29@gmail.com>
2019-08-19 12:07:45 +0300

    add github fork logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ import * as actions from '../store/actions';
 
 
 import logo from '../assets/logo.jpg'
+import forkLogo from '../assets/github.png';
 
 function Header() {
 
@@ -26,10 +27,16 @@ function Header() {
 				<Link to='/'>
 					<img onClick={resetCompanies} src={logo} alt="" style={{ height: 80, cursor: 'pointer' }} />
 				</Link>
-				<div className="d-flex align-items-center">
+				<div className="d-flex align-items-center mr-5">
 					<Link to='/istatistik' className="btn btn-dark px-5 mr-3 darkmode-ignore" style={{ fontSize: 16 }}>İstatistikler</Link>
 					<a rel="noopener noreferrer" className="btn btn-success px-5 darkmode-ignore" target="_blank" style={{ fontSize: 16 }} href="https://github.com/frontendistanbul/technologies">Yeni Şirket Ekle</a>
 				</div>
+			</div>
+
+			<div className='fork' >
+			<a href="https://github.com/eserdinyo/frontend-tr">
+				<img src={forkLogo} alt=""/>
+			</a>
 			</div>
 		</div>
 	)
